7abd6d374a64cc87d982935644e0c71a92618f01
Mohammadhosein <mohammadh.josh@proton.me>
2026-09-15 17:54:39 +0330

    fixed some spacing

--- JobQueueSystem/Messaging/NATS.go
+++ JobQueueSystem/Messaging/NATS.go
@@ -1,1 +1,18 @@
 package messaging
+
+import "github.com/nats-io/nats.go"
+
+
+
+func Connect() (*nats.Conn , error){
+	return nats.Connect(nats.DefaultURL)
+}
+
+
+func Publish(nc *nats.Conn, subject string, data []byte) error {
+	return nc.Publish(subject, data)
+}
+
+func Subscribe(nc *nats.Conn, subject string, handler nats.MsgHandler) (*nats.Subscription, error) {
+	return nc.Subscribe(subject, handler)
+}
